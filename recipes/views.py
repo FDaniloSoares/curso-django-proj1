f73f17f1e118b054fa20e187bf1d7b9893d7103b
Danilo Soares <fdanilosoares@gmail.com>
2022-10-21 12:55:27 -0300

    include recepis app

---
+++ recipes/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "recipes/home.html", context={"name": "Danilo"})
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
