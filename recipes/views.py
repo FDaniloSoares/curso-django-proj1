3ddc2d4fc96b5b10ad391a34ceb8fadaddbc4a16
Danilo Soares <fdanilosoares@gmail.com>
2022-10-22 17:39:38 -0300

    finalize layout

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "recipes/home.html", context={"name": "Danilo"})
+    return render(request, "recipes/pages/home.html", context={"name": "Danilo"})
 
 
 def contato(request):
